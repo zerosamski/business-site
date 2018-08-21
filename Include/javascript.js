36d17ef4c82e98ec58d3be7d62c07b7ab3aca8de
Samuel Mulkens <sammulkens@gmail.com>
2018-08-21 15:02:25 +0200

    Made first version of header

---
+++ Include/javascript.js
@@ -0,0 +1,9 @@
+$(document).ready(() => {
+    
+	$('.dropdown-menu').hover(function () {
+    	$('.dropdown-toggle').dropdown()
+    });
+});
+	/*	function () {
+    	$(this).removeClass('newcolor');
+	*/
