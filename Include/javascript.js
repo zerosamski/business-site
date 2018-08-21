30e7a58aeaed1aad0b58964acc21337d94988ed2
Samuel Mulkens <sammulkens@gmail.com>
2018-08-21 17:24:31 +0200

    Changed button and dropdown size, added hover effect

--- Include/javascript.js
+++ Include/javascript.js
@@ -1,9 +1,16 @@
-$(document).ready(() => {
-    
-	$('.dropdown-menu').hover(function () {
-    	$('.dropdown-toggle').dropdown()
-    });
+$('ul.navbar-nav li.boards').hover(function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeIn(100);
+}, function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeOut(100);
 });
-	/*	function () {
-    	$(this).removeClass('newcolor');
-	*/
+
+$('ul.navbar-nav li.accessories').hover(function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeIn(100);
+}, function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeOut(100);
+});
+
+
+
+
+
