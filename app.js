a1bbbe0fdcf5f651581a5d76425fd85f7b3145ea
Samuel Mulkens <sammulkens@gmail.com>
2018-08-27 15:33:18 +0200

    localhost assignment

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+
+app.use(express.static('root'));
+
+app.use('/images', express.static(__dirname + '/Images'));
+app.use('/include', express.static(__dirname + '/Include'));
+
+
+app.listen(3000, () =>
+	console.log('Server has started!'))
+
